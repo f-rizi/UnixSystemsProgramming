720cc2509c0e14f68aa3cea2c2be69cf20ec6cdf
f-rizi <5946983+f-rizi@users.noreply.github.com>
2025-05-03 15:36:22 -0700

    makeargv.c added

---
+++ ch02/argtest.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int makeargv(const char *str, const char *delimiters, char ***argvp);
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s string\n", argv[0]);
+        return 1;
+    } 
+
+    char delimiters[] = " \t";
+    char **myargv;
+    int numtokens;
+
+    if ((numtokens = makeargv(argv[1], delimiters, &myargv)) == -1) {
+        fprintf(stderr, "Failed to construct an argument array for %s\n", argv[1]);
+        return 1;
+    }
+
+    printf("The argument array contains:\n");
+
+    for (int i = 0; i < numtokens; i++) {
+        printf("%d:%s\n", i, myargv[i]);
+    }
+
+    return 0;
+}
---
+++ ch02/makeargv.c
@@ -0,0 +1,56 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+int makeargv(const char *str, const char *delimiters, char ***argvp) {
+    if (str == NULL || delimiters == NULL || argvp == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    int error;
+    int numtokens;
+    const char *strnew;
+    char *strcp;
+    *argvp = NULL;
+
+    strnew = str + strcspn(str, delimiters);
+
+    if ((strcp = malloc(strlen(strnew) + 1)) == NULL) {
+        return -1;
+    }
+
+    strcpy(strcp, strnew);
+    numtokens = 0;
+
+    if (strtok(strcp, delimiters) != NULL) {
+        numtokens += 1;
+        while (strtok(NULL, delimiters) != NULL)
+        {
+            numtokens += 1;
+        }        
+    }
+
+    if ((*argvp = malloc(numtokens * sizeof(char *))) == NULL) {
+        error = errno;
+        free(strcp);
+        errno = error;
+        return -1;
+    }
+
+    if (numtokens == 0) {
+        free(strcp);
+        *(*argvp) = NULL;
+        return 0;
+    }
+
+    strcpy(strcp, strnew);
+    **argvp = strtok(strcp, delimiters);
+
+    for (int i = 1; i < numtokens; i++) {
+        *((*argvp) + i) = strtok(NULL, delimiters);
+    }
+
+    *(*(argvp) + numtokens) = NULL;
+    return numtokens;
+}
