9a36569ef853492752850b0d00dea1684bec1f42
f-rizi <5946983+f-rizi@users.noreply.github.com>
2025-05-03 21:24:31 -0700

    wordaverage.c and wordaveragetest.c added

---
+++ ch02/wordaverage.c
@@ -0,0 +1,40 @@
+#include <string.h>
+
+#define LINE_DELIMITER "\n"
+#define WORD_DELIMITER " "
+
+static int countwords(char *line) {
+    char *last;
+
+    if (strtok_r(line, WORD_DELIMITER, &last) == NULL) {
+        return 0;
+    } 
+
+    int counter = 1;
+
+    while (strtok_r(NULL, WORD_DELIMITER, &last) != NULL) {
+        counter += 1;
+    }
+
+    return counter;
+}
+
+double wordaverage(char *lines) {
+    char *last;
+
+    char *line = strtok_r(lines, LINE_DELIMITER, &last);
+
+    if (line == NULL) {
+        return 0.0;
+    }
+
+    int linecounter = 1;
+    int wordcounter = countwords(line);
+
+    while ((line = strtok_r(NULL, LINE_DELIMITER, &last)) != NULL) {
+        linecounter += 1;
+        wordcounter += countwords(line);
+    }
+
+    return (double)wordcounter / linecounter;
+}
---
+++ ch02/wordaveragetest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double wordaverage(const char *str);
+
+int main(int argc, char *argv[]) {
+    
+    char str[] = "Hello, World!\n\nThis is a string with multiple newlines\nand spaces.";
+    printf("word average is %f\n", wordaverage(str));
+
+    return 0;
+}
