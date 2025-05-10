8db64dafb5b31e0fd5a35820fa73da888eedfd55
f-rizi <5946983+f-rizi@users.noreply.github.com>
2025-05-10 11:07:27 -0700

    use r-read and r-write to copy files

---
+++ ch04/copyfile.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "../appendixB/restart.h"
+
+#define BLKSIZE 1024
+
+
+int copyfile(int fromfd, int tofd) {
+    char buf[BLKSIZE];
+    int bytesread;
+    int byteswrite;
+    int totalbytes = 0;
+
+    for (;;) {
+        if ((bytesread = r_read(fromfd, buf, BLKSIZE)) <= 0) {
+            break;
+        }
+
+        if ((byteswrite = r_write(tofd, buf, bytesread)) == -1) {
+            break;
+        }
+
+        totalbytes += byteswrite;
+    }
+
+    return totalbytes;
+}
