5a3f6468ee87970731112b616770d707b143c548
f-rizi <5946983+f-rizi@users.noreply.github.com>
2025-05-19 18:05:30 -0700

    redirect using pipe, fork and dup2

--- ch06/parentwritepipe.c
+++ ch06/parentwritepipe.c
@@ -4,6 +4,16 @@
 #include <sys/types.h>
 #define BUFSIZE 10
 
+/*
+the output should be something like : 
+./parentwritepipe 
+[2644437]:my bufin is {Empty}, my bufout is {Hello}
+[2644438]:my bufin is {Hello}, my bufout is {Hello}
+
+However, if the write in the parent fails or the read 
+in the child retrieves a partial result, then the output
+will be different.
+*/
 int main(void)
 {
     char bufin[BUFSIZ] = "Empty";
