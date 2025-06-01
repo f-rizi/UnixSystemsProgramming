b448a8a484cd7dd5638141f7ce595c2a9dbccca4
f-rizi <5946983+f-rizi@users.noreply.github.com>
2025-05-31 23:10:46 -0700

    use threads to copy file

---
+++ ch12/copyfilepass.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "../appendixB/restart.h"
+
+void *copyfilepass(void *arg)
+{
+    int *argint;
+
+    argint = (int *)arg;
+    argint[2] = copyfile(argint[0], argint[1]);
+    
+    r_close(argint[0]);
+    r_close(argint[1]);
+
+    return argint + 2;
+}
