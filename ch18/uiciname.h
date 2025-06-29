a7590fb98e9153d1bab3dc5f4a877967cf2cf528
f-rizi <5946983+f-rizi@users.noreply.github.com>
2025-06-29 16:49:12 -0700

    2 versions of server - simple and fork

---
+++ ch18/uiciname.h
@@ -0,0 +1,13 @@
+/* uiciname.h  name resolution functions */
+
+#include <netinet/in.h>
+#include <sys/types.h>
+
+#define REENTRANT_NONE 0
+#define REENTRANT_R 1
+#define REENTRANT_MUTEX 2
+#define REENTRANT_POSIX 3
+
+int name2addr(char *name, in_addr_t *addrp);
+void addr2name(struct in_addr addr, char *name, int namelen);
+ 
